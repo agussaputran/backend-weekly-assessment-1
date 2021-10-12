84b40ae0310a671bb43f329016e5415f5190f0a6
Gerry Pratama <gerryajie@gmail.com>
2021-10-12 15:28:58 +0700

    add file index from Gerry

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function getFullname(firstName, middleName, lastName) {
-  const fullName = `Full name : ${firstName} ${middleName} ${lastName}`;
+function getFullname(firstName, middleName, lastName, age) {
+  const fullName = `Nama Saya : ${firstName} ${middleName} ${lastName} dan umur ${age} Tahun`;
   return fullName;
 }
 
-console.log(getFullname("Gerry", "Ajie", "Pratama"));
+console.log(getFullname("Gerry", "Ajie", "Pratama", 35));

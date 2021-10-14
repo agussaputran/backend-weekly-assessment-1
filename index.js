d2b38965907ca206689305a94e6867e13214e044
Nushair Falah <lendsham1st@gmail.com>
2021-10-14 17:13:19 +0700

    add file

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 function getFullname(firstName, middleName, lastName) {
-    //process
-    const result = `Full Name: ${firstName} ${middleName} ${lastName}`;
-    // nilai balikan
-    return result;
+  //process
+  const result = `Full Name: ${firstName} ${middleName} ${lastName}`;
+  // nilai balikan
+  return result;
 }
 
 // call the function
 console.log(getFullname("Nushair", "Falah", "Izzaty"))
-=======
-function getFullname(paramater1, parameter2) {
-  //process
-  // nilai balikan
-}
-
-// call the function
-console.log(getFullname(parameter1, parameter2));
->>>>>>> 09377e189d25979e2b98633b334bdced5cf4e6b3

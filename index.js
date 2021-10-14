550964210ac02234548fcd6eecfd21183e3b7d03
Daniel Togar Harianja <88548845+dantori99@users.noreply.github.com>
2021-10-14 17:26:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ function getFullname(paramater1, parameter2) {
 }
 
 // call the function
-<<<<<<< HEAD
-console.log(getFullname(paramater1, parameter2));
-=======
-console.log(getFullname(parameter1, parameter2));
->>>>>>> parent of f6b7b95... Merge pull request #6 from agussaputran/daniel_harianja
